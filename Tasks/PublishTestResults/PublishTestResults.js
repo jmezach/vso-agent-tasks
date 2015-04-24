087565a5e5199660c73e38ca54df6d77ad9acefe
Madhuri Gummalla <madhurig@microsoft.com>
2015-04-24 18:11:08 -0400

    draft

---
+++ Tasks/PublishTestResults/PublishTestResults.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var glob = require('glob');
+var tl = require('vso-task-lib');
+var console = require('console');
+
+var testRunner = tl.getInput('testRunner', true);
+var testResultsFiles = tl.getInput('testResultsFiles', true);
+var mergeResults = tl.getInput('mergeTestResults');
+
+tl.debug('testRunner: ' + testRunner);
+tl.debug('testResultsFiles: ' + testResultsFiles);
+tl.debug('mergeResults: ' + mergeResults);
+
+var onError = function(errorMsg) {
+	tl.error(errorMsg);
+	tl.exit(1);
+}
+
+//check for pattern in testResultsFiles
+if(testResultsFiles.indexOf('*') >= 0 || testResultsFiles.indexOf('?') >= 0) {
+	tl.debug('Pattern found in testResultsFiles parameter');
+	var matchingTestResultsFiles = glob.sync(testResultsFiles);
+	tl.debug('matchingTestResultsFiles = ' + matchingTestResultsFiles);
+}
+else {
+	tl.debug('No pattern found in testResultsFiles parameter');
+	var matchingTestResultsFiles = [testResultsFiles];
+}
+
+if(!matchingTestResultsFiles) {
+	onError('No test results files with search pattern ' + testResultsFiles + ' were found.');
+}
+
+for(var i = 0; i < matchingTestResultsFiles.length; i ++) {
+	tl.debug('Reading results from: ' + matchingTestResultsFiles[i]);
+	console.log('##vso[results.publish type=JUnit]' + matchingTestResultsFiles[i]);
+}
